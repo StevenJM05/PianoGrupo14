6a6fa2a0fb34acad253246ff7596be086b633152
Carlos Sandoval <antonio.sandoval22@itca.edu.sv>
2024-10-15 22:14:44 -0600

    Piano Tradicional

--- app/src/main/java/sv/edu/itca/pianogrupo14/Piano_tradicional.java
+++ app/src/main/java/sv/edu/itca/pianogrupo14/Piano_tradicional.java
@@ -1,6 +1,9 @@
 package sv.edu.itca.pianogrupo14;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,4 +24,46 @@ public class Piano_tradicional extends AppCompatActivity {
             return insets;
         });
     }
+    private void MessageBox(String mensaje) {
+        Toast.makeText(this,mensaje,Toast.LENGTH_SHORT).show();
+    }
+
+    public void d_o(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.n_do);
+        ndo.start();
+        MessageBox("DO");
+    }
+    public void re(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.re);
+        ndo.start();
+        MessageBox("RE");
+
+    }
+    public void mi(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.mi);
+        ndo.start();
+        MessageBox("MI");
+    }
+    public void fa(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.fa);
+        ndo.start();
+        MessageBox("FA");
+    }
+    public void sol(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.sol);
+        ndo.start();
+        MessageBox("SOL");
+    }
+    public void la(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.la);
+        ndo.start();
+        MessageBox("LA");
+    }
+    public void si(View view){
+        MediaPlayer ndo=MediaPlayer.create(this,R.raw.si);
+        ndo.start();
+        MessageBox("SI");
+
+    }
+
 }
